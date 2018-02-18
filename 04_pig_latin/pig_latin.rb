280e013318130b1683e4f0fe975bac0c1c0a8b0b
Daniel Varcas <daniel.varcas@gmail.com>
2018-02-18 23:40:44 +0000

    minor refactor

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,8 +1,7 @@
 def translate(string)
 	vowels = ["a", "e", "i", "o", "u"]
 	string.split.map! do |string|
-		string = string[1..-1] + string[0] until vowels.include?(string[0])
-		string = string[1..-1] + string[0] if (string[0] == "u" && string[-1] == "q")
-		string = string + "ay"
+		string = string[1..-1] + string[0] until vowels.include?(string[0]) && ((string[0] == "u" && string[-1] == "q") == false)
+		string += "ay"
 	end.join(" ")
 end

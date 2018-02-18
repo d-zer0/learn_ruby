fda8af13a1ec3af3209e545dd2946d2a133185c3
Daniel Varcas <daniel.varcas@gmail.com>
2018-02-18 23:34:11 +0000

    pig latin works

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,8 +1,8 @@
 def translate(string)
 	vowels = ["a", "e", "i", "o", "u"]
-
-	string = string[1..-1] + string[0] until vowels.include?(string[0])
-
-	string + "ay"
-	#vowels.include?(string[0]) ? string + "ay" : string = string[1..-1] + string[0]
+	string.split.map! do |string|
+		string = string[1..-1] + string[0] until vowels.include?(string[0])
+		string = string[1..-1] + string[0] if (string[0] == "u" && string[-1] == "q")
+		string = string + "ay"
+	end.join(" ")
 end
